7d806fd3461caa682348295d22710f8b27e2254b
Damir <damirmatematic@gmail.com>
2026-05-31 14:16:08 +0300

    Remade functions so they fits tree.hpp

--- src/tree.cpp
+++ src/tree.cpp
@@ -10,17 +10,27 @@ Node::Node(const std::string& key,const std::string& value):
         {};
 
 
-unsigned int Node::height()
+unsigned int AVLTree::height(Node *p)
 {
-    return h;
+    
+    return p?p->h:0;
 }
 
-int Node::bfactor()
+int AVLTree::bfactor(Node *p)
 {
-    unsigned int hl = left? left->height():0;
-    unsigned int hr = right? right->height():0;
+    unsigned int hl = p->left? height(p->left):0;
+    unsigned int hr = p->right? height(p->right):0;
 
-    return static_cast<int>(hl)-static_cast<int>(hr);
+    return static_cast<int>(hr)-static_cast<int>(hl);
 }
+
+void AVLTree::fixheight(Node *p)
+{
+    unsigned int hl = height(p->left);
+    unsigned int hr = height(p->right);
+
+    p->h = (hl>hr?hl:hr)+1;
+}
+
 
 
