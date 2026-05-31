0576b4bcf31229eb056e1183f20f3b5be5b3868b
Damir <damirmatematic@gmail.com>
2026-05-31 16:35:41 +0300

    Banned copying

---
+++ tree/tree.hpp
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <string>
+
+class Node
+{
+    private:
+    std::string key;
+    std::string value;
+    Node* left;
+    Node* right;
+    unsigned int h;
+
+    Node(const std::string& key,const std::string& value);
+    ~Node() = default;
+    friend class AVLTree;
+};
+
+class AVLTree
+{
+    private:
+    Node* root;
+    unsigned int height(Node *p);
+    int bfactor(Node *p);
+    void fixheight(Node *p);
+
+    Node* RotateRight(Node *p);
+    Node* RotateLeft(Node *p);
+    Node* balance(Node *p);
+    Node* insert(Node *p,const std::string& key,const std::string& value);
+    Node* find(Node *p,const std::string& key);
+    void clear(Node *p);
+    void print(Node *p,int& number)const;
+    public:
+    bool get(const std::string& key,std::string& value);
+    std::string& operator[](const std::string& key);
+    void print()const;
+    AVLTree();
+    ~AVLTree();
+};
