79ec9f17529d0ca60865158a43539d763474e0da
Damir <damirmatematic@gmail.com>
2026-05-31 14:02:44 +0300

    Made functions more accurate

--- src/tree.cpp
+++ src/tree.cpp
@@ -1,10 +1,26 @@
 #include "tree.hpp"
-#include <string>
 
-Node::Node(const std::string& key,const std::string& value):key(key),
-                                                            value(value)
-                                                            left(nullptr),
-                                                            right(nullptr),
-                                                            h(1){};
-                                                            
+
+Node::Node(const std::string& key,const std::string& value):
+        key(key),                                               
+        value(value),                                              
+        left(nullptr),                                            
+        right(nullptr),                                                            
+        h(1)
+        {};
+
+
+unsigned int Node::height()
+{
+    return h;
+}
+
+int Node::bfactor()
+{
+    unsigned int hl = left? left->height():0;
+    unsigned int hr = right? right->height():0;
+
+    return static_cast<int>(hl)-static_cast<int>(hr);
+}
+
 
